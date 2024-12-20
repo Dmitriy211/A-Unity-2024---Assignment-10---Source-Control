759ce1b080b1f8151ff8e6f33d9969ba336c9fbf
yerkanat <91059141+yerkanatn@users.noreply.github.com>
2024-12-20 23:58:29 +0500

    Code for Bighorn
    Paste the files in Core/Data/Resources/Animals/ or unzip and replace Animals File

---
+++ AnimalData.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+/* not working, kinda similar to Eagle creation code
+[CreateAssetMenu(fileName = "AnimalData", menuName = "Custom/Animal", order = 0)]
+public class AnimalData : ScriptableObject
+{
+    [field:SerializeField] public AnimalStats Stats { get; private set; }
+    //insert prefab in below field in Unity Editor
+    [field:SerializeField] public GameObject AnimalVisuals { get; private set; }
+}
+
+
+[Serializable]
+public struct AnimalStats
+{
+    public string name;
+    public float speed;
+}
+*/
